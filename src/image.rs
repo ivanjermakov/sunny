22315e315de7cabcc7ed0a0e57d5c656ea95c3cb
ivanjermakov <ivanjermakov1@gmail.com>
2023-01-28 16:13:43 +0100

    Ppm format fix

--- src/image.rs
+++ src/image.rs
@@ -12,16 +12,13 @@ pub struct Image {
 impl Image {
     pub fn save_ppm(&self, path: &str) -> std::io::Result<()> {
         let content = format!(
-            "P3\n{w} {h}\n15\n{d}",
+            "P3\n{w} {h}\n255\n{d}",
             w = self.resolution.x,
             h = self.resolution.y,
             d = self
                 .pixels
                 .iter()
-                .map(|Color { r, g, b }| format!("{r:<3} {g:<3} {b:<3}"))
-                .collect::<Vec<_>>()
-                .chunks(self.resolution.x as usize)
-                .map(|c| c.join("   "))
+                .map(|Color { r, g, b }| format!("{r} {g} {b} "))
                 .collect::<Vec<_>>()
                 .join("\n")
         );
@@ -36,13 +33,17 @@ mod test {
     use crate::vec3::Vec3;
 
     #[test]
-    fn save_solid_image() {
-        let r = Vec3::new(600., 400., 0.);
+    fn save_image() {
+        let r = Vec3::new(100., 100., 0.);
         let w = r.x as i32;
         let mut ps = vec![];
-        for _ in 0..w as i32 {
-            for _ in 0..r.y as i32 {
-                ps.push(Color { r: 255, g: 0, b: 0 });
+        for x in 0..w as i32 {
+            for y in 0..r.y as i32 {
+                ps.push(Color {
+                    r: if (x + y) % 2 == 0 { 255 } else { 0 },
+                    g: 0,
+                    b: 0,
+                });
             }
         }
         let i = Image {
